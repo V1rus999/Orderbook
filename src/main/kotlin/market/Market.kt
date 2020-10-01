ba4bb5f107130ec065b17cea1a2ae68168e9fe59
Johannes <h-annie@live.co.za>
2020-10-01 09:14:06 +0200

    Renaming order for clarity

--- src/main/kotlin/market/Market.kt
+++ src/main/kotlin/market/Market.kt
@@ -5,25 +5,26 @@ package market
  * @Date: 2020-09-28, Mon
  **/
 class Market(
-    private val orderBook: OrderBook = OrderBook()
+    private val orderBook: OrderBook = OrderBook(),
+    private val completedOrders: List<LimitOrder> = listOf()
 ) {
-    fun handleLimitOrder(order: LimitOrder): String {
+    fun handleLimitOrder(incomingOrder: LimitOrder): String {
         val topBuy = orderBook.retrieveBestBuyPrice()
         val topSell = orderBook.retrieveBestSellPrice()
 
-        val shouldOrderBeAdded = shouldOrderBeAddedToBookDirectly(order, topBuy, topSell)
+        val shouldOrderBeAdded = shouldOrderBeAddedToBookDirectly(incomingOrder, topBuy, topSell)
         if (shouldOrderBeAdded) {
             //This should return result of type ORDERMATCHED. Probably need to calculate how much has been depleted
-            orderBook.addNewTrade(order)
+            orderBook.addNewTrade(incomingOrder)
         } else {
-            depleteOrder(order)
+            depleteOrder(incomingOrder)
         }
 
         return "Done"
     }
 
-    private fun depleteOrder(order: LimitOrder) {
-        var orderBeingCompleted = order.copy()
+    private fun depleteOrder(incomingOrder: LimitOrder) {
+        var orderBeingCompleted = incomingOrder.copy()
         var orderBookDepleted = false
 
         while (orderBeingCompleted.quantity > 0.0.toBigDecimal() && !orderBookDepleted) {
@@ -42,31 +43,34 @@ class Market(
         }
     }
 
-    private fun matchOrderWithExistingBookOrder(newOrder: LimitOrder, existingBookOrder: LimitOrder): LimitOrder {
-        return if (existingBookOrder.quantity > newOrder.quantity) {
-            val newTopBuy = existingBookOrder.copy(quantity = existingBookOrder.quantity - newOrder.quantity)
+    private fun matchOrderWithExistingBookOrder(incomingOrder: LimitOrder, existingBookOrder: LimitOrder): LimitOrder {
+        return if (existingBookOrder.quantity > incomingOrder.quantity) {
+            val newTopBuy = existingBookOrder.copy(quantity = existingBookOrder.quantity - incomingOrder.quantity)
             //TODO add these orders to a completed orders list
             //TODO need to return a result object
             orderBook.modifyTopLimit(newTopBuy)
-            newOrder.copy(quantity = 0.toBigDecimal())
+            incomingOrder.copy(quantity = 0.toBigDecimal())
         } else {
             orderBook.removeTopLimit(existingBookOrder)
-            newOrder.copy(quantity = newOrder.quantity - existingBookOrder.quantity)
+            incomingOrder.copy(quantity = incomingOrder.quantity - existingBookOrder.quantity)
         }
     }
 
+    //TODO it might be better to do this directly in the orderbook
     private fun shouldOrderBeAddedToBookDirectly(
-        order: LimitOrder,
+        incomingOrder: LimitOrder,
         topBuy: LimitOrder?,
         topSell: LimitOrder?,
     ): Boolean {
         return if (topBuy == null && topSell == null) {
             true
-        } else if (topBuy != null && order.side == "BUY" && topBuy.price >= order.price) {
+        } else if (topBuy != null && incomingOrder.side == "BUY" && topBuy.price >= incomingOrder.price) {
             true
-        } else topSell != null && order.side == "SELL" && topSell.price <= order.price
+        } else topSell != null && incomingOrder.side == "SELL" && topSell.price <= incomingOrder.price
     }
 
     fun retrieveCurrentOrderBook(): OrderBook = orderBook
+
+    fun retrieveOrderList(): List<LimitOrder> = completedOrders
 
 }
